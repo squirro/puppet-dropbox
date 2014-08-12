10117eacbaac31915f3d0c81aea0acb8b708bbf9
Marco Silva <igamads@gmail.com>
2014-08-12 10:31:48 +0200

    update init.d script and install it

--- manifests/config.pp
+++ manifests/config.pp
@@ -1,3 +1,12 @@
 class dropbox::config {
 
+    file { "/etc/init.d/dropbox":
+      ensure  => $logstash::ensure,
+      source => 'puppet:///modules/dropbox/etc/init.d/dropbox.debian',
+      owner   => 'root',
+      group   => 'root',
+      mode    => '0755',
+      notify  => Service['dropbox'],
+    }
+
 }
--- manifests/package.pp
+++ manifests/package.pp
@@ -14,18 +14,21 @@ class dropbox::package {
     managehome => 'true',
     comment    => 'Dropbox Service Account',
   }
+
   group { $dropbox::params::dx_gid:
     ensure => 'present',
   }
 
   exec { 'download-dropbox':
-    command => "wget -O /tmp/dropbox.tar.gz \"http://www.dropbox.com/download/?plat=lnx.${download_arch}\"",
+    command => "wget -O /tmp/dropbox.tar.gz \"https://www.dropbox.com/download/?plat=lnx.${download_arch}\"",
     require => User[$dropbox::params::dx_uid],
   }
+
   exec { 'install-dropbox':
     command => 'tar -zxvf /tmp/dropbox.tar.gz -C ~${dropbox::params::dx_uid}',
     require => Exec['download-dropbox'],
   }
+
   file { '/tmp/dropbox.tar.gz':
     ensure  => 'absent',
     require => Exec['install-dropbox'],
