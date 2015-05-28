9b4f762abdc58a6f5ac06e1fa09a384c4e0a20dd
Patrice Neff <mail@patrice.ch>
2015-05-28 17:08:13 +0200

    Handle multiple users

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,5 +1,9 @@
 class dropbox (
+  # The user into which the Dropbox binary will be installed.
   $user = 'dropbox-user',
+  # The user(s) for which Dropbox will be started. Specify multiple users to
+  # run multiple Dropbox instances on one server.
+  $instances = ['dropbox-user'],
   ) {
   include stdlib
 
